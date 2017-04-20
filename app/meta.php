4d7dea40f99ba464913d54bbd8f637f99588208a
eritikass <eritikass@gmail.com>
2017-04-20 16:30:48 +0300

    move all page meta to single file

---
+++ app/meta.php
@@ -0,0 +1,7 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="description" content="">
+<meta name="author" content="">
+
+<title><?php echo getenv('CAPRITOOLS_TITLE') ? getenv('CAPRITOOLS_TITLE') : 'NOG8S Tools (courtesy of Capri\'s Tools)'; ?></title>
